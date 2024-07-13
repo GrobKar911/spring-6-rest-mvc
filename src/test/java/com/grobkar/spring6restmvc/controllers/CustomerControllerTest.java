10744f703d943ce1ea3713f2ac4b19c09549b815
48516 <grobkar91@gmail.com>
2024-07-13 11:53:04 +0200

    Development: added test for create customer

--- src/test/java/com/grobkar/spring6restmvc/controllers/CustomerControllerTest.java
+++ src/test/java/com/grobkar/spring6restmvc/controllers/CustomerControllerTest.java
@@ -1,8 +1,10 @@
 package com.grobkar.spring6restmvc.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.grobkar.spring6restmvc.model.Customer;
 import com.grobkar.spring6restmvc.services.CustomerService;
 import com.grobkar.spring6restmvc.services.CustomerServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,8 +13,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(CustomerController.class)
@@ -21,10 +25,33 @@ class CustomerControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
     @MockBean
     CustomerService customerService;
 
-    CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
+    CustomerServiceImpl customerServiceImpl;
+
+    @BeforeEach
+    void setUp() {
+        customerServiceImpl = new CustomerServiceImpl();
+    }
+
+    @Test
+    void createCustomer() throws Exception {
+        Customer customer = customerServiceImpl.listAllCustomers().get(0);
+        customer.setCustomerId(null);
+        customer.setVersion(null);
+
+        given(customerService.newSavedCustomer(any(Customer.class))).willReturn(customerServiceImpl.listAllCustomers().get(1));
+
+        mockMvc.perform(post("/api/v1/customer")
+                .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(customer)))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"));
+    }
 
     @Test
     void listCustomers() throws Exception {
